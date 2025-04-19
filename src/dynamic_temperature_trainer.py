79445d7c690918b81af0df05e7484e7034ff3505
Jus1mple <1226448285.com>
2025-04-19 21:32:13 +0800

    Initial commit

---
+++ src/dynamic_temperature_trainer.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+# @ Start Time: 04/12/2025
+# @ Last Modified: 04/15/2025
+# Author: Kedong Xiu
+
+from transformers import AutoTokenizer, Trainer, AutoModelForCausalLM
+import torch
+import torch.nn as nn
